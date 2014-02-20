5ce79f87cfd89a020b3f1ebca5353afbb24b65cb
scottishkilt <theinsidiousbookworm@gmail.com>
2014-02-20 15:47:11 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+webdev-skeleton
+===============
+
+Getting started:
+
+0. Get and install [npm](https://www.npmjs.org/doc/README.html), the [node.js](http://nodejs.org/download/) package manager.
+1. Use npm to install Grunt: (sudo) `npm install -g grunt-cli`
+2. Clone this repository if you haven't already
+2. Change directory to the root of this project, and install dependencies with `npm install`
+3. Still at the project root, run a webserver with `grunt server`
+4. Open a text editor and edit `index.html`, `js/main.js`, and (optionally) `css/main.css`
---
+++ js/main.js
@@ -0,0 +1,104 @@
+
+var width = 1000,
+    height = 700
+	
+var color = d3.scale.category20();
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var force = d3.layout.force()
+    .gravity(.05)
+    .distance(100)
+    .charge(-100)
+    .size([width, height]);
+	
+
+
+var filePath = "cooccurrence_lol.json";
+var alpha = .01;
+d3.json( filePath, function( data) {
+  var cooc = data.matrix;
+  var backBoned = [];
+  for(var i =cooc.length-1; i>=0; i--){
+	backBoned[i]=[];
+	var k_n=cooc[i].length;
+	if(k_n >1){
+		var sum_w =0;
+		for(var j=cooc[i].length-1; j>=0; j--){
+			sum_w+=cooc[i][j];
+		}
+		for(var j=cooc[i].length-1; j>=0; j--){
+			var edgeWeight=cooc[i][j];
+			var pij= edgeWeight/sum_w;
+			if( Math.pow((1-pij),(k_n-1)) < alpha){
+				backBoned[i][j]=edgeWeight;
+			}
+			else{
+				backBoned[i][j]=0;
+			}
+		}
+	}
+  }
+	 
+	 var nodes=[], links=[];
+	 for(var i = data.labels.length-1; i>=0; i--){
+		nodes.push({name: data.labels[i], group: Math.floor(Math.random()*10), size: Math.random()*10+1});
+	 }
+	 for(var i = 0; i<backBoned.length; i++){
+		for(var j =0; j<i; j++){
+			if(backBoned[i][j]>0){
+				links.push({source: i,target:j,value: backBoned[i][j]});
+				}
+			}
+		}
+		var json={nodes: nodes,
+				  links: links
+				  };
+//});
+
+
+
+	
+//"cooccurrence_sexual_activity_backbone_d3.json"
+//d3.json("cooccurrence_sexual_activity_backbone_d3.json", function(error, json) {
+
+  force
+      force.nodes(json.nodes)
+      .links(json.links)
+      .start();
+
+  var link = svg.selectAll(".link")
+      .data(json.links)
+    .enter().append("line")
+      .attr("class", "link");
+
+
+var node = svg.selectAll(".node")
+    .data(json.nodes)
+  .enter().append("g")
+    .attr("class", "node")
+	.attr("r", 10)
+	.style("fill", function(d) { return color(d.group); })
+    .call(force.drag);
+
+node.append("circle")
+    .attr("r", function(d){ return Math.log(d.size);})
+	.style("fill", function(d) { return color(d.group); });
+
+node.append("text")
+    .attr("dx", function(d){ return Math.log(d.size);})
+    .attr("dy", ".45em")
+    .text(function(d) { return d.name })
+	.style("font-size",function(d){  return Math.log(d.size)*4;});
+
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  });
+});
